ca5aa0a67efc1a55a30bc2cde3bd55d749922d0b
Kodellektor <a7torres@ucsd.edu>
2023-08-04 09:47:02 -0700

    modified gitignore and bot

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import random
 from random import randint, sample
 import os
 from dotenv import load_dotenv
+import diceroni
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,9 +19,10 @@ async def on_ready():
 @client.command()
 async def hi(ctx):
     await ctx.send('Hello!')
-
+    
 @client.command()
 async def roll(ctx, dice: str):
+    
     """ Outputs the roll (number of dice | d | dicefaces/dicetype in NdN format)
     As of 07/23/2023 I have yet to handle non standard dice ex: 1d3, modifiers or advantage"""
     
@@ -28,11 +30,24 @@ async def roll(ctx, dice: str):
     
     try:
         numdie, dietype = map(int, dice.split('d'))
-    except Exception:
+        
+    except Exception: #Being lazy on this until I take time to narrow and figure exact errors
         await ctx.send('Please retry your request in the format NdN!')
         return
 
     result = ', '.join(str(random.randint(1, dietype)) for n in range(numdie))
     await ctx.send(f'Fate has granted you: {result}')
+
+@client.command()
+async def encounter(ctx, partystrength: str, difficulty: str):
+    
+    """Creates an encounter based on party size and level at the difficulty chosen"""
+
+    
+
+
+
+
+
 
 client.run(TOKEN)
